6016f959d6a578e2e2a821866d2648b281823a3d
av-mukhortova <av.mukhortova@gmail.com>
2025-04-08 11:44:32 +0300

    fix: add dependencies

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,6 +1,5 @@
 import { Request } from 'express';
-import { JwtPayload } from 'jsonwebtoken';
 
 export interface SessionRequest extends Request {
-    user?: string | JwtPayload;
+    user: any;
 }
